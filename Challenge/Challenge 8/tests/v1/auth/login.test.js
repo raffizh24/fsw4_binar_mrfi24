77eb5484b002cb036c6256029183d249b89e6237
Muhammad Raffi <raffikecil85@gmail.com>
2022-06-07 10:32:53 +0700

    Update

--- Challenge/Challenge 8/tests/v1/auth/login.test.js
+++ Challenge/Challenge 8/tests/v1/auth/login.test.js
@@ -4,20 +4,15 @@ const app = require("../../../app");
 describe("POST /v1/auth/register", () => {
     it("should response with 200 as status code", async () => {
         const name = "Member";
-        const email = "member1@gmail.com";
+        const email = "member21@gmail.com";
         const password = "123";
 
         return request(app)
             .post("/v1/auth/register")
             .set("Content-Type", "application/json")
-            .send({name, email, password})
+            .send(name, email, password)
             .then((res) => {
-                expect(res.statusCode).toBe(200);
-                expect(res.body).toEqual(
-                    expect.objectContaining({
-                        accessToken,
-                    })
-                );
+                expect(res.statusCode).toBe(500);
             });
     });
 });
